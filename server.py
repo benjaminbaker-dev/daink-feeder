9b2619f0f5a7fc99db055080a1a176ccd55a52ba
benjaminbaker-dev <benjaminbakercty@gmail.com>
2019-08-01 19:07:03 +0300

    improved functionality - added funcs for reading and writing to JSON

--- server.py
+++ server.py
@@ -9,10 +9,23 @@ RANK_COUNT_KEY = "rank_count"
 DANK_COUNT_KEY = "dank_count"
 
 
-def is_ranked(file_id):
-    with open(RANK_JSON, "r") as f:
-        ranked_files = json.load(f)
-    return file_id in ranked_files.keys()
+def _read_rank_json() -> dict:
+    try:
+        with open(RANK_JSON, "r") as f:
+            ranked_files = json.load(f)
+        return ranked_files
+    except json.JSONDecodeError:
+        return {}
+
+
+def _write_rank_json(rank_json: dict):
+    with open(RANK_JSON, "w") as f:
+        json.dump(rank_json, f)
+
+
+def _is_ranked(file_id):
+    rank_json = _read_rank_json()
+    return file_id in rank_json.keys()
 
 
 def get_unranked_file_name():
@@ -20,7 +33,7 @@ def get_unranked_file_name():
     file_id = random.choice(files)
     try_counter = 1
 
-    while is_ranked(file_id):
+    while _is_ranked(file_id):
         if try_counter == len(files):
             raise ValueError("no more memes !")
 
@@ -30,15 +43,12 @@ def get_unranked_file_name():
 
 
 def update_file_id_counter(file_id, key):
-    with open(RANK_JSON, "r") as f:
-        ranked_files = json.load(f)
+    rank_json = _read_rank_json()
 
     try:
-        ranked_files[file_id][key] += 1
+        rank_json[file_id][key] += 1
     except KeyError:
-        ranked_files[file_id] = {}
-        ranked_files[file_id][key] = 1
+        rank_json[file_id] = {}
+        rank_json[file_id][key] = 1
 
-    with open(RANK_JSON, "w") as f:
-        json.dump(ranked_files, f)
-
+    _write_rank_json(rank_json)
