59b80cb83ab1cc87d401ff02f79f5c5e278d58f6
benjaminbaker-dev <benjaminbakercty@gmail.com>
2019-08-01 19:15:15 +0300

    removed uneccessary is_rank check

---
+++ ranking.py
@@ -0,0 +1,41 @@
+import os
+import json
+import random
+
+MEME_FOLDER = r"/Users/benjaminbaker/Desktop/meme dataset"
+RANK_JSON = r"/Users/benjaminbaker/Desktop/memeranking.json"
+
+RANK_COUNT_KEY = "rank_count"
+DANK_COUNT_KEY = "dank_count"
+
+
+def _read_rank_json() -> dict:
+    try:
+        with open(RANK_JSON, "r") as f:
+            ranked_files = json.load(f)
+        return ranked_files
+    except json.JSONDecodeError:
+        return {}
+
+
+def _write_rank_json(rank_json: dict):
+    with open(RANK_JSON, "w") as f:
+        json.dump(rank_json, f)
+
+
+def get_file_name():
+    files = os.listdir(MEME_FOLDER)
+    file_id = random.choice(files)
+    return file_id
+
+
+def update_file_id_counter(file_id, key):
+    rank_json = _read_rank_json()
+
+    try:
+        rank_json[file_id][key] += 1
+    except KeyError:
+        rank_json[file_id] = {}
+        rank_json[file_id][key] = 1
+
+    _write_rank_json(rank_json)
--- server.py
+++ server.py
@@ -1,54 +1,6 @@
-import os
-import json
-import random
+from flask import Flask
 
-MEME_FOLDER = r"/Users/benjaminbaker/Desktop/meme dataset"
-RANK_JSON = r"/Users/benjaminbaker/Desktop/memeranking.json"
-
-RANK_COUNT_KEY = "rank_count"
-DANK_COUNT_KEY = "dank_count"
+app = Flask(__name__)
 
 
-def _read_rank_json() -> dict:
-    try:
-        with open(RANK_JSON, "r") as f:
-            ranked_files = json.load(f)
-        return ranked_files
-    except json.JSONDecodeError:
-        return {}
-
-
-def _write_rank_json(rank_json: dict):
-    with open(RANK_JSON, "w") as f:
-        json.dump(rank_json, f)
-
-
-def _is_ranked(file_id):
-    rank_json = _read_rank_json()
-    return file_id in rank_json.keys()
-
-
-def get_unranked_file_name():
-    files = os.listdir(MEME_FOLDER)
-    file_id = random.choice(files)
-    try_counter = 1
-
-    while _is_ranked(file_id):
-        if try_counter == len(files):
-            raise ValueError("no more memes !")
-
-        file_id = random.choice(files)
-        try_counter += 1
-    return file_id
-
-
-def update_file_id_counter(file_id, key):
-    rank_json = _read_rank_json()
-
-    try:
-        rank_json[file_id][key] += 1
-    except KeyError:
-        rank_json[file_id] = {}
-        rank_json[file_id][key] = 1
-
-    _write_rank_json(rank_json)
+@app.route("/"):
